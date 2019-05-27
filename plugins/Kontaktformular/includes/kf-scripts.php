c862b99fe225e1d33b45b97a0dc29c1b5ae35a4a
sedlackovave68356 <sedlackovave68356@th-nuernberg.de>
2019-05-27 13:57:59 +0200

    Upload New File

---
+++ plugins/Kontaktformular/includes/kf-scripts.php
@@ -0,0 +1,16 @@
+<?php
+
+// Fügt Scripts in die Warteschlange hinzu
+function kf_add_scripts()
+{
+    //snippet für javaScript Bibliothek, immer vor der java-datei!
+    wp_register_script('js-snippet', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js');
+    wp_enqueue_script('js-snippet');
+    //JS hinzufügen
+    wp_enqueue_script('kf-main-script', plugins_url(). '/Kontaktformular/js/main.js');
+    // CSS 
+    //   wp_enqueue_style('kf-main-style', '/wp-content/plugins/Kontaktformular/css/style.css');
+
+}
+add_action('wp_enqueue_scripts','kf_add_scripts');
+?>
